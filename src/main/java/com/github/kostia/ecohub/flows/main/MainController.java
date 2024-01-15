e677bff7411d64bb6bde9bb7cfccb6ab780e8ba3
mstepan <maksym.stepanenko@oracle.com>
2024-01-15 15:56:23 +0200

    Login form added

--- src/main/java/com/github/kostia/ecohub/flows/main/MainController.java
+++ src/main/java/com/github/kostia/ecohub/flows/main/MainController.java
@@ -1,6 +1,5 @@
 package com.github.kostia.ecohub.flows.main;
 
-import com.github.kostia.ecohub.config.EcohubConfig;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -11,18 +10,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 
-    private final EcohubConfig ecohubConfig;
-
-    public MainController(EcohubConfig ecohubConfig) {
-        this.ecohubConfig = ecohubConfig;
-    }
-
     @GetMapping("/")
     public String main(HttpServletRequest request, Model model) {
-
-//        HttpSession session = request.getSession(true);
-        model.addAttribute("idToken", "my-cool-idtoken");
-
         return "index";
     }
 
---
+++ src/main/java/com/github/kostia/ecohub/security/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.github.kostia.ecohub.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class SecurityConfig{
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+
+        UserDetails admin = User.builder()
+            .username("admin")
+            .password(passwordEncoder().encode("admin"))
+            .roles("ADMIN")
+            .build();
+
+        return new InMemoryUserDetailsManager(admin);
+    }
+
+    @Bean
+    public static PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
