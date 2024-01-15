966f1f2c5868e0dbcee6b8aeabe7c0a91e03f9e0
mstepan <maksym.stepanenko@oracle.com>
2024-01-15 09:58:30 +0200

    Location basic functionality added

---
+++ src/main/java/com/github/kostia/ecohub/flows/location/LocationController.java
@@ -0,0 +1,18 @@
+package com.github.kostia.ecohub.flows.location;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Slf4j
+@Controller
+public class LocationController {
+
+    @GetMapping("/location")
+    public String location(Model model) {
+
+        return "location";
+    }
+
+}
