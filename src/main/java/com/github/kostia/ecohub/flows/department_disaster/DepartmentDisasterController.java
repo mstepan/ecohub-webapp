6d30de31c441b8c15a3e2584d432ca02e967cef9
mstepan <maksym.stepanenko@oracle.com>
2024-01-16 11:01:42 +0200

    DepartmentDisaster completed

--- src/main/java/com/github/kostia/ecohub/flows/department_disaster/DepartmentDisasterController.java
+++ src/main/java/com/github/kostia/ecohub/flows/department_disaster/DepartmentDisasterController.java
@@ -1,7 +1,9 @@
 package com.github.kostia.ecohub.flows.department_disaster;
 
+import com.github.kostia.ecohub.flows.department.DepartmentRepo;
 import com.github.kostia.ecohub.flows.disaster.Disaster;
 import com.github.kostia.ecohub.flows.disaster.DisasterRepo;
+import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.relational.core.conversion.DbActionExecutionException;
 import org.springframework.stereotype.Controller;
@@ -16,46 +18,75 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class DepartmentDisasterController {
 
 
+    private final DepartmentDisasterRepo depDisasterRepo;
+
     private final DisasterRepo disasterRepo;
 
-    public DepartmentDisasterController(DisasterRepo disasterRepo) {
+    private final DepartmentRepo departmentRepo;
+
+    public DepartmentDisasterController(DepartmentDisasterRepo depDisasterRepo, DisasterRepo disasterRepo,
+                                        DepartmentRepo departmentRepo) {
+        this.depDisasterRepo = depDisasterRepo;
         this.disasterRepo = disasterRepo;
+        this.departmentRepo = departmentRepo;
     }
 
     @GetMapping("/department_disaster")
-    public String departmentDisaster(Model model) {
-        fillDataModel(model);
+    public String departmentDisaster(@RequestParam("departmentId") String departmentId, Model model, HttpSession session) {
+
+        setDepartmentId(departmentId, session);
+
+        fillDataModel(model, session);
 
         return "department_disaster";
     }
 
     @PostMapping("/department_disaster")
-    public String addDepartmentDisaster(@ModelAttribute Disaster disaster, Model model) {
-        disasterRepo.save(disaster);
+    public String addDepartmentDisaster(@ModelAttribute DepartmentDisaster departmentDisaster, Model model, HttpSession session) {
 
-        fillDataModel(model);
+        departmentDisaster.setDepartmentId(getDepartmentId(session));
+
+        depDisasterRepo.save(departmentDisaster);
+
+        fillDataModel(model, session);
 
         return "department_disaster";
     }
 
     @GetMapping("/department_disaster/delete")
-    public String deleteDepartmentDisaster(@RequestParam("disasterId") String disaster, Model model) {
+    public String deleteDepartmentDisaster(@RequestParam("departmentDisasterId") String depDisasterId, Model model, HttpSession session) {
+
+        DepartmentDisaster depDis = depDisasterRepo.findById(Integer.parseInt(depDisasterId)).get();
+
         try {
-            disasterRepo.deleteById(Integer.parseInt(disaster));
+            depDisasterRepo.deleteById(Integer.parseInt(depDisasterId));
         }
         catch(DbActionExecutionException dbEx){
             log.error("Can't delete disaster for department");
             model.addAttribute("errorMessage", "Can't delete disaster for department");
         }
 
-        fillDataModel(model);
+        fillDataModel(model, session);
 
         return "department_disaster";
     }
 
-    private void fillDataModel(Model model){
-        model.addAttribute("departmentDisaster", Disaster.builder().build());
-        model.addAttribute("allDepartmentDisasters", disasterRepo.findAll());
+    private void setDepartmentId(String departmentId, HttpSession session){
+        session.setAttribute("departmentId", departmentId);
+    }
+
+    private Integer getDepartmentId(HttpSession session){
+        return Integer.parseInt((String)session.getAttribute("departmentId"));
+    }
+
+    private void fillDataModel(Model model, HttpSession session){
+
+        Integer curDepId = getDepartmentId(session);
+
+        model.addAttribute("department", departmentRepo.findById(curDepId).get());
+        model.addAttribute("allDisasters", disasterRepo.findAll());
+        model.addAttribute("departmentDisaster", DepartmentDisaster.builder().build());
+        model.addAttribute("allDepartmentDisasters", depDisasterRepo.findAllDisastersForDepartmentId(curDepId));
     }
 
 }
--- src/main/java/com/github/kostia/ecohub/flows/department_disaster/DepartmentDisasterRepo.java
+++ src/main/java/com/github/kostia/ecohub/flows/department_disaster/DepartmentDisasterRepo.java
@@ -1,11 +1,20 @@
 package com.github.kostia.ecohub.flows.department_disaster;
 
+import com.github.kostia.ecohub.flows.department.Department;
 import com.github.kostia.ecohub.flows.disaster.Disaster;
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface DepartmentDisasterRepo extends CrudRepository<DepartmentDisaster, Integer>{
+
+    @Query("SELECT * FROM department_disaster WHERE department_id = :departmentId")
+    List<DepartmentDisaster> findAllDisastersForDepartmentId(@Param("departmentId") Integer departmentId);
+
 }
 
 
