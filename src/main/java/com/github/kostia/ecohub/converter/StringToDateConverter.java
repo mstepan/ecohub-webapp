39e16db30660b8eca872f3eb4a3788aee677e421
mstepan <maksym.stepanenko@oracle.com>
2024-01-16 15:35:35 +0200

    JobHistory fully completed

--- src/main/java/com/github/kostia/ecohub/converter/StringToDateConverter.java
+++ src/main/java/com/github/kostia/ecohub/converter/StringToDateConverter.java
@@ -1,4 +1,4 @@
-package com.github.kostia.ecohub.flows.employee;
+package com.github.kostia.ecohub.converter;
 
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.data.convert.ReadingConverter;
@@ -8,6 +8,9 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+/**
+ * Converts String to Date type.
+ */
 @Component
 @ReadingConverter
 public class StringToDateConverter implements Converter<String, Date> {
@@ -17,6 +20,9 @@ public class StringToDateConverter implements Converter<String, Date> {
     @Override
     public Date convert(String str) {
         try {
+            if( str == null || "".equals(str.trim()) ){
+                return null;
+            }
             return dateFormat.parse(str);
         } catch (ParseException e) {
             throw new IllegalArgumentException("Invalid date format", e);
